3a70322605bce5504cea510385f84fe5e2b27846
Alex Yashin <alex-yashin@ya.ru>
2023-08-01 12:08:14 +0400

    уточняю тип данных телефона

--- src/Types/PhoneType.php
+++ src/Types/PhoneType.php
@@ -2,13 +2,33 @@
 
 namespace PinaUsers\Types;
 
+use Pina\Html;
 use Pina\Types\StringType;
+use Pina\Types\ValidateException;
+
+use function Pina\__;
 
 class PhoneType extends StringType
 {
     public function getSize(): int
     {
-        return 32;
+        return 64;
+    }
+
+    public function draw($value): string
+    {
+        return Html::a(parent::draw($value), 'tel:+' . $value);
+    }
+
+    public function normalize($value, $isMandatory)
+    {
+        $value = parent::normalize($value, $isMandatory);
+
+        $value = preg_replace('/[^\d]/', '', $value ?? '');
+        if ((empty($value) || strlen($value) <= 3) && $isMandatory) {
+            throw new ValidateException(__("Укажите телефон"));
+        }
+        return $value;
     }
 
 }
