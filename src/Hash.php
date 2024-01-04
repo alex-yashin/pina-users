f1920f7329e4147fe068e95809d84adc3d4dea54
Alex Yashin <alex-yashin@ya.ru>
2024-01-04 13:23:32 +0400

    делаю возможно перегружать логику вычисления хеша пароля

--- src/Hash.php
+++ src/Hash.php
@@ -2,18 +2,23 @@
 
 namespace PinaUsers;
 
+use Pina\App;
+
 class Hash
 {
 
     public static function make($password)
     {
-        $salt = substr(str_replace('+', '.', base64_encode(sha1(microtime(true), true))), 0, 22);
-        return crypt($password, '$2a$12$' . $salt);
+        /** @var Model\Hash $impl */
+        $impl = App::load(Model\Hash::class);
+        return $impl->make($password);
     }
 
     public static function check($password, $hash)
     {
-        return $hash == crypt($password, $hash);
+        /** @var Model\Hash $impl */
+        $impl = App::load(Model\Hash::class);
+        return $impl->check($password, $hash);
     }
 
 }
